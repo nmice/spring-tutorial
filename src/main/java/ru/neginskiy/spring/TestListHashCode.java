f3e9228d87e890e25023fcdf1ef4fc43921a6bc8
nmice <n.mice@mail.ru>
2019-03-27 18:48:37 +0300

    test collection hashcode()

---
+++ src/main/java/ru/neginskiy/spring/TestListHashCode.java
@@ -0,0 +1,23 @@
+package ru.neginskiy.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestListHashCode {
+
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        System.out.println(list.hashCode());
+        list.add("123");
+        list.add("23");
+        System.out.println(list.hashCode());
+
+        List list2 = new ArrayList();
+        System.out.println(list2.hashCode());
+        list2.add("123");
+        list2.add("23");
+        System.out.println(list2.hashCode());
+
+        System.out.println(list.equals(list2));
+    }
+}
